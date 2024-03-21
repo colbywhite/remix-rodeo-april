74f58db9add0b1a6666df0be89cb9560a6639b47
Seth Davis <sethdavis512@gmail.com>
2024-03-20 21:07:02 -0500

    First commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { vitePlugin as remix } from '@remix-run/dev';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [
-    remix({
-      ssr: false,
-    }),
-    tsconfigPaths(),
-  ],
+    base: '/remix-rode-april/',
+    plugins: [
+        remix({
+            basename: '/remix-rode-april/',
+            ssr: false
+        }),
+        tsconfigPaths()
+    ]
 });
